6eaf77620ae08121f8e549c2a1c65125e0986e3b
Tensai1713 <fenx2332@gmail.com>
2024-10-21 16:30:45 +0300

    v10

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ menuIcon.addEventListener('click', () => {
   header.classList.toggle('header-mobile');
   body.classList.toggle('no-scroll');
 } );
+
+
+const modal = document.querySelector('.modal__wrap')
+const btnsModelOpen = document.querySelectorAll('.btn[type="button"]')
+
+btnsModelOpen.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    modal.classList.remove('none');
+    body.classList.add('no-scroll');
+  });
+})
+
+const btnModalExit = document.querySelector('.modal__exit')
+
+btnModalExit.addEventListener('click',() => {
+    modal.classList.add('none');
+    body.classList.remove('no-scroll');
+})
